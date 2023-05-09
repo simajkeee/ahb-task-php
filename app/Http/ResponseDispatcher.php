1750815d7bc3fb4e960a0f503447c31ce81bd559
simajkeee <37087698+simajkeee@users.noreply.github.com>
2023-05-09 12:48:47 +0300

    change Access-Control-Max-Age header value

--- app/Http/ResponseDispatcher.php
+++ app/Http/ResponseDispatcher.php
@@ -13,9 +13,8 @@ class ResponseDispatcher
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Credentials: true");
         header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-        header('Access-Control-Max-Age: 1000');
+        header('Access-Control-Max-Age: 3600');
         header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
-//        header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
         if ($response instanceof JsonResponse) {
             header("Content-Type: application/json");
